64adeaa0d14bcb7b13bad55c712abc82950dc9d9
Pinky Gupta <pinkkygupta1@gmail.com>
2026-08-11 19:02:27 +0530

    done param task

---
+++ Express/param.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+const port = 4000;
+
+app.get("/welcome/:username", (req, res) => {
+    const username = req.params.username;
+    const role = req.query.role;
+
+    res.send(`Welcome ${username}, your role is ${role}`);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
