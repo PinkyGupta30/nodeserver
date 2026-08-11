e45c05e3a34b36a9cd81db18a21bae8d36b8eded
Pinky Gupta <pinkkygupta1@gmail.com>
2026-08-11 16:42:03 +0530

    Build Express server with orders and users routes

---
+++ Express/Routes.js
@@ -0,0 +1,24 @@
+const express = require("express");
+let port = 3000;
+const app = express();
+app.get("/orders",(req,res)=>
+{
+    res.send("Here is the list of all orders");
+})
+app.post("/orders",(req,res)=>
+{
+    res.send("A new order has been created");
+})
+app.get("/users",(req,res)=>
+{
+    res.send("Here is the list of all users");
+})
+app.post("/users",(req,res)=>
+{
+    res.send("A new user has been added");
+})
+
+app.listen(port,()=>
+{
+    console.log("server is running");
+})
