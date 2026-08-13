6c19d8282742d449ee3292de9726b6596d1467d9
Pinky Gupta <pinkkygupta1@gmail.com>
2026-08-13 19:55:13 +0530

    controller task done

---
+++ ecommAPI/controller/productController.js
@@ -0,0 +1,19 @@
+const getProducts = (req,res)=>
+{
+    res.send("Fetching all products");
+}
+const addProducts = (req,res)=>
+{
+    res.send("Adding a new Product");
+}
+
+const productById = (req, res)=>{
+    const id = req.params.id;
+    res.send(`Fetching product with ID: ${id}`);
+}
+
+module.exports = {
+    getProducts,
+    addProducts,
+    productById
+};
--- ecommAPI/routes/productRoutes.js
+++ ecommAPI/routes/productRoutes.js
@@ -1,19 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-console.log("Product routes loaded");
+const productController = require("../controller/productController");
 
-router.get("/", (req, res) => {
-    res.send("Fetching all products");
-});
+router.get("/", productController.getProducts);
 
-router.post("/", (req, res) => {
-    res.send("Adding a new product");
-});
+router.post("/", productController.addProducts);
 
-router.get("/:id", (req, res) => {
-    const id = req.params.id;
-    res.send(`Fetching product with ID: ${id}`);
-});
+router.get("/:id", productController.productById);
 
 module.exports = router;
